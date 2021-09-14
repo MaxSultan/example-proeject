bb7c64d094b08c0282ec714049ac24ea9c1924a4
Maxwell Sultan <maxwellsultan@Maxwells-MacBook-Pro.local>
2021-09-14 09:29:34 -0600

    landing page now broken down into components

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import NavBar from '../../components/Layout/NavBar';
+import Footer from '../../components/Layout/Footer';
+import Contact from '../../components/Layout/Contact';
+import HeroSection from '../../components/Layout/HeroSection';
+import LogoCloud from '../../components/Layout/LogoCloud';
+import AlternatingFeatureSection from '../../components/Layout/AlternatingFeatureSection';
+import Stats from '../../components/Layout/Stats';
+import Features from '../../components/Layout/Features';
 
 export interface HomeProps {
   title: string;
 }
 
-const Home = ({ title }: HomeProps) => (
-  <h1 className="my-8 max-w-2xl mx-auto">{title}</h1>
+const Home = () => (
+  <>
+    <div className="bg-white">
+      <NavBar />
+      <main>
+        <HeroSection />
+        <LogoCloud />
+        <AlternatingFeatureSection />
+        <Features />
+        <Stats />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  </>
 );
 
 export default Home;
-
-export const getStaticProps = () => ({
-  props: {
-    title: 'Hello world',
-    layout: {
-      metadata: {
-        title: 'Example Application',
-      },
-    },
-  },
-});
